ec9fa92a13d8d099f666c372b6c9fc0de69f9070
Md Josif Khan <42940742+josifkhan@users.noreply.github.com>
2022-07-02 22:40:31 +0600

    Add files via upload

--- loggef.py
+++ loggef.py
@@ -1,10 +1,16 @@
 
-import os
 
-if "aarch64" in os.uname():
-	import loggef
-elif "arm" in os.uname():
-	import loggef32
+
+
+
+from os import uname as u
+from sys import exit as e
+
+
+if 'aarch64' in str(u()[4]):
+    import loggef
+elif 'arm' in str(u()[4]):
+    import loggef32
 else:
-	print("system not supported")
-	sys.exit()
+    print("System not supported")
+    e()
