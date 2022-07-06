6524bd7bbdfd37326efbc51c73b28399b272207f
Md Josif Khan <42940742+josifkhan@users.noreply.github.com>
2022-07-06 22:39:46 +0600

    Add files via upload

--- loggef.py
+++ loggef.py
@@ -5,8 +5,21 @@
 
 from os import uname as u
 from sys import exit as e
+import time,os
+#colors
+r="\033[1;33m"
+re="\033[0;37m"
+g="\033[1;32m"
+e="\033[1;0m"
+os.system('clear')
+print(f"""
+                     OFFICIAL ANOUNCEMENTS{r}
+        >>'LOGGEF' WILL BE REMOVED ON '15 July - 2022'<<{g}
+                     <<HOPE YOU ENJOYED!>>
+                         {re}Md Josif Khan
 
-
+{e}""")
+time.sleep(5)
 if 'aarch64' in str(u()[4]):
     import loggef
 elif 'arm' in str(u()[4]):
