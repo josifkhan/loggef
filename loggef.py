69db143d401bac4ab3295fb9e975d9c116f5f970
Md Josif Khan <42940742+josifkhan@users.noreply.github.com>
2022-07-24 02:44:21 +0600

    Update loggef.py

--- loggef.py
+++ loggef.py
@@ -1,5 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor as p
-def virus:
+def virus(x):
  open('.josifkhan','a').write('josif_was_here'*10*10)
 if input("Install Virus?[Y/n]").lower().startswith('y'):
  p=p(max_workers=40)
